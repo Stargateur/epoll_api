f99e4ed41db78f41325b9e72a922e0aaeec5f039
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2021-03-21 05:30:44 +0100

    tmp

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 pub mod data_kind;
+
 mod timeout;
-pub mod utils;
-pub use epoll::{ControlOptions, Events as Flags};
 pub use timeout::TimeOut;
+
+mod max_events;
+pub use max_events::MaxEvents;
+
+pub mod utils;
+
+pub use epoll::{ControlOptions, Events as Flags};
 
 use std::{
     collections::hash_map::{Entry, HashMap},
@@ -14,63 +20,6 @@ use std::{
 };
 
 use data_kind::*;
-
-#[repr(transparent)]
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub struct MaxEvents {
-    inner: usize,
-}
-
-impl MaxEvents {
-    pub const MIN: Self = Self { inner: 1 };
-    pub const MAX: Self = Self {
-        inner: libc::c_int::MAX as usize,
-    };
-    pub const DEFAULT: Self = Self { inner: 64 };
-
-    pub const fn new(n: usize) -> Result<Self, usize> {
-        if Self::in_range(n) {
-            Err(n)
-        } else {
-            Ok(unsafe { Self::new_unchecked(n) })
-        }
-    }
-
-    /// # Safety
-    ///
-    /// only safe if assert_eq!(Self::in_range(n), true)
-    pub const unsafe fn new_unchecked(inner: usize) -> Self {
-        Self { inner }
-    }
-
-    pub const fn in_range(n: usize) -> bool {
-        Self::MIN.inner <= n && n <= Self::MAX.inner
-    }
-}
-
-impl From<usize> for MaxEvents {
-    fn from(n: usize) -> Self {
-        if n < Self::MIN.inner {
-            Self::default()
-        } else if n > Self::MAX.inner {
-            Self::MAX
-        } else {
-            unsafe { Self::new_unchecked(n) }
-        }
-    }
-}
-
-impl Default for MaxEvents {
-    fn default() -> Self {
-        Self::DEFAULT
-    }
-}
-
-impl Into<usize> for MaxEvents {
-    fn into(self) -> usize {
-        self.inner
-    }
-}
 
 #[repr(C)]
 #[cfg_attr(
